7af3b0a0c121d79bfbddcb602ec747645e9666d2
hektop <souzaheitto@gmail.com>
2025-03-08 16:07:48 -0300

    Update desafioiniciante.c
    adiçao do if else

--- desafioiniciante.c
+++ desafioiniciante.c
@@ -1,63 +1,78 @@
 #include <stdio.h>
-    struct Carta{
-        int populacao;
-        float area;
-        float pib;
-        int pontos_turisticos;
 
-    };
+struct Carta {
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+};
 
-        int main(){
-            struct Carta carta1, carta2;
+int main() {
+    struct Carta carta1, carta2;
+    int escolha;
 
-            //DADOS DA CARTA 1
-            
-                    printf("Digite os dados da carta\n");
-            
-                    printf("Populaçao: ");
-                    scanf("%d", &carta1.populacao);
-                    
-                    printf("Área em km²: ");
-                    scanf("%f", &carta1.area);
-                    
-                    printf("PIB da carta: ");
-                    scanf("%f", &carta1.pib);
-            
-                    printf("Quantidade de pontos turisticos: ");
-                    scanf("%d", &carta1.pontos_turisticos);
-            
-            //DADOS DA CARTA 2 
-            
-                printf("Digite os dados da segunda carta\n");
-                printf("População: ");
-                scanf("%d", &carta2.populacao);
-            
-                printf("Área em km²: ");
-                scanf("%f", &carta2.area);
-            
-                printf("PIB da carta: ");
-                scanf("%f", &carta2.pib);
-            
-                printf("Quantidade de pontos turisticos: ");
-                scanf("%d", &carta2.pontos_turisticos);
-            
-                // EXIBIÇÃO DOS DADOS //
-            
-                printf("\nDados da primeira carta:\n");
-                printf("Populacao: %d\n", carta1.populacao);
-                printf("Area: %.2f km²\n", carta1.area);
-                printf("PIB: R$ %.2f\n", carta1.pib);
-                printf("Pontos turisticos: %d\n", carta1.pontos_turisticos);
-            
-                // EXIBIÇÃO DOS DADOS 2 //
-            
-                printf("\nDados da segunda carta:\n");
-                printf("Populacao: %d\n", carta2.populacao);
-                printf("Area: %.2f km²\n", carta2.area);
-                printf("PIB: R$ %.2f\n", carta2.pib);
-                printf("Pontos turisticos: %d\n", carta2.pontos_turisticos);
-            
-                return 0;
-            
-            
+    // Entrada de dados da primeira carta
+    printf("Digite os dados da primeira carta:\n");
+    printf("População: ");
+    scanf("%d", &carta1.populacao);
+    printf("Área em km²: ");
+    scanf("%f", &carta1.area);
+    printf("PIB: ");
+    scanf("%f", &carta1.pib);
+    printf("Pontos turísticos: ");
+    scanf("%d", &carta1.pontos_turisticos);
+
+    // Entrada de dados da segunda carta
+    printf("\nDigite os dados da segunda carta:\n");
+    printf("População: ");
+    scanf("%d", &carta2.populacao);
+    printf("Área em km²: ");
+    scanf("%f", &carta2.area);
+    printf("PIB: ");
+    scanf("%f", &carta2.pib);
+    printf("Pontos turísticos: ");
+    scanf("%d", &carta2.pontos_turisticos);
+
+    // Escolha do atributo para comparação
+    printf("\nEscolha um atributo para comparar:\n");
+    printf("1 - População\n2 - Área\n3 - PIB\n4 - Pontos turísticos\n");
+    scanf("%d", &escolha);
+
+    if (escolha == 1) {
+        if (carta1.populacao > carta2.populacao) {
+            printf("A primeira carta tem maior população.\n");
+        } else if (carta1.populacao < carta2.populacao) {
+            printf("A segunda carta tem maior população.\n");
+        } else {
+            printf("As cartas têm a mesma população.\n");
         }
+    } else if (escolha == 2) {
+        if (carta1.area > carta2.area) {
+            printf("A primeira carta tem maior área.\n");
+        } else if (carta1.area < carta2.area) {
+            printf("A segunda carta tem maior área.\n");
+        } else {
+            printf("As cartas têm a mesma área.\n");
+        }
+    } else if (escolha == 3) {
+        if (carta1.pib > carta2.pib) {
+            printf("A primeira carta tem maior PIB.\n");
+        } else if (carta1.pib < carta2.pib) {
+            printf("A segunda carta tem maior PIB.\n");
+        } else {
+            printf("As cartas têm o mesmo PIB.\n");
+        }
+    } else if (escolha == 4) {
+        if (carta1.pontos_turisticos > carta2.pontos_turisticos) {
+            printf("A primeira carta tem mais pontos turísticos.\n");
+        } else if (carta1.pontos_turisticos < carta2.pontos_turisticos) {
+            printf("A segunda carta tem mais pontos turísticos.\n");
+        } else {
+            printf("As cartas têm o mesmo número de pontos turísticos.\n");
+        }
+    } else {
+        printf("Opção inválida.\n");
+    }
+
+    return 0;
+}
